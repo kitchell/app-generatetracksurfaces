283c5ab666dfcbdb0ddaa74230ecd61882436b07
kitchell <lindseykitchell21@gmail.com>
2017-06-01 15:36:43 -0400

    update

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from niftiMask2Surface import niftiMask2Surface
 with open('config.json') as config_json:
     config = json.load(config_json)
 
+pwd = os.getcwd()
+os.mkdir(pwd + "/surfaces")
+os.chdir(pwd + "/surfaces")
+
+
 for file in glob.glob(config["maskdir"] + "/*Vol.nii.gz"):
     split_name = os.path.basename(file).split('_')
     split_name[-1] = 'surf.ply'
