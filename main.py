dc762c6fcc29e84dec7c32cc29e2717f76532e9d
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-06-13 13:12:56 -0400

    Create main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ os.chdir(pwd + "/surfaces")
 print('looking for ' + config["maskdir"] + "/*Vol.nii.gz")
 
 for file in glob.glob(config["maskdir"] + "/*Vol.nii.gz"):
+    print(file)
     split_name = os.path.basename(file).split('_')
     split_name[-1] = 'surf.vtk'
     surfname = '_'.join(split_name)
+    print(surfname)
     niftiMask2Surface(file, surfname, 10)
