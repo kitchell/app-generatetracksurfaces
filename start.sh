d0eca1a0d36fc9bdf8d75058acfaba3082ccc4e7
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-08-03 11:07:48 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,21 @@ if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
 #clean up previous job (just in case)
 rm -f finished
-module unload python/2.7.3
-module load anaconda2
+
+
+if [ $ENV == "IUHPC" ]; then
+	module unload python
+	if [ $HPC == "KARST" ]; then
+		module load anaconda2
+	fi
+	if [ $HPC == "CARBONATE" ]; then
+		module load anaconda/python2.7/4.3.1
+	fi
+	
+fi
+	
+	#module unload python/2.7.3
+
 
 # remove surface directory
 rm -rf surfaces
