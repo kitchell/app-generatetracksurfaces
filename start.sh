b7565dcca89638cf1f4eae30147b679f2997f6ca
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-21 12:07:26 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ nohup time python $SERVICE_DIR/main.py > stdout.log 2> stderr.log
 #if [ -s Right_Uncinate_surf.vtk ];
 
 count=$(ls surfaces/* | wc -l)
-if [ $count -eq $NUMFILES ] 
+if [ $count == $NUMFILES ] 
 then
 	echo 0 > finished
 else
