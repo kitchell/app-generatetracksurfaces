3628ba3dd4e1d4101f13ad16653be9e2668d1791
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-21 11:49:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ filetype = config["filetype"]
 for file in glob.glob(config["maskdir"] + "/*Vol.nii.gz"):
     print(file)
     split_name = os.path.basename(file).split('_')
-    split_name[-1] = 'surf.vtk'
+    split_name[-1] = 'surf.' + filetype
     surfname = '_'.join(split_name)
     print(surfname)
     niftiMask2Surface(file, 'surfaces/'+surfname, 10, filetype)
