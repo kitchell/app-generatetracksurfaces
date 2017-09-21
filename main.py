25428d9d11cb6f6c0fbf98a2244f6000f81983bc
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-21 11:44:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ os.mkdir(pwd + "/surfaces")
 #os.chdir(pwd + "/surfaces")
 
 print('looking for ' + config["maskdir"] + "/*Vol.nii.gz")
-
+filetype = config["filetype"]
 for file in glob.glob(config["maskdir"] + "/*Vol.nii.gz"):
     print(file)
     split_name = os.path.basename(file).split('_')
     split_name[-1] = 'surf.vtk'
     surfname = '_'.join(split_name)
     print(surfname)
-    niftiMask2Surface(file, 'surfaces/'+surfname, 10)
+    niftiMask2Surface(file, 'surfaces/'+surfname, 10, filetype)
