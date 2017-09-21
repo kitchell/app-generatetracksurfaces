41f55acc12970c9dee50c13a8558aa3cc05d71b6
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-21 12:31:07 -0400

    create numfiles.py

---
+++ numfiles.py
@@ -0,0 +1,9 @@
+import os
+import fnmatch
+import json
+
+with open('config.json') as config_json:
+    config = json.load(config_json)
+
+numfiles = len(fnmatch.filter(os.listdir(config["maskdir"]), '*.nii.gz'))
+print numfiles
