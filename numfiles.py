435d87f005c5d9e8d8dc97472931e258d3a45017
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-27 15:55:05 -0400

    Update numfiles.py

--- numfiles.py
+++ numfiles.py
@@ -7,5 +7,5 @@ import json
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-numfiles = len(fnmatch.filter(os.listdir(config["maskdir"]), '*.nii.gz'))
+numfiles = len(fnmatch.filter(os.listdir(config["maskdir"]), '*.nii.gz')) + 1
 print numfiles
